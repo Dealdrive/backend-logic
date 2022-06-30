c5bf5fbea7cb033da91723f1e0162fe3b95e3318
johnayuk <john1234ayuk@gmail.com>
2022-06-30 14:05:17 +0100

    chaged names of pakages and category in services

--- service/models.py
+++ service/models.py
@@ -10,7 +10,7 @@ def package_image_path(instance, filename):
     return 'image/pak/{}/{}'.format(instance.name, filename)
 
 class Category(models.Model):
-    name = models.CharField(max_length=250, null=True, blank=True)
+    categoryName = models.CharField(max_length=250, null=True, blank=True)
     image = models.ImageField(upload_to= category_image_path, null=True, blank = True )
     discription = models.TextField(max_length=500, null=True, blank=True)
     # created = models.DateTimeField(auto_now_add=True,null=True, blank=True)
@@ -24,7 +24,7 @@ class Category(models.Model):
 
 
 class Package(models.Model):
-    name = models.CharField(max_length=250)
+    packageName = models.CharField(max_length=250)
     image = models.ImageField(upload_to= package_image_path, null=True, blank = True )
     price = models.DecimalField(decimal_places=2, max_digits=10, null=True, blank=True)
     description = models.TextField(max_length=500, null=True, blank=True)
