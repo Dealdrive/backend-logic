c1dd4444de7c08415e5b52f99f3c79b46df62f47
johnayuk <john1234ayuk@gmail.com>
2022-06-30 12:35:08 +0100

    new features

--- dealdrive/settings.py
+++ dealdrive/settings.py
@@ -14,12 +14,12 @@ import os
 import django_heroku
 from decouple import config
 from pathlib import Path
-import environ
+# import environ
 
 
 # Initialise environment variables
-os.env = environ.Env()
-environ.Env.read_env()
+# os.env = environ.Env()
+# environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -29,8 +29,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-1par1f1s)-w(1ay20i0)9fbugxdp&cbb-!@@^m*@8)w=kfal)1'
-SECRET_KEY = os.env('SECRET_KEY')
+SECRET_KEY = 'django-insecure-1par1f1s)-w(1ay20i0)9fbugxdp&cbb-!@@^m*@8)w=kfal)1'
+# SECRET_KEY = os.env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
